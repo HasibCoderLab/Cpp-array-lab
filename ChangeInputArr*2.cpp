a1023bb416497a0b32d92f3c0985b7f01ae89689
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-07 23:45:39 +0600

    create a file for practice Array

---
+++ ChangeInputArr*2.cpp
@@ -0,0 +1,15 @@
+// ================================================
+//               Language: C++ / Cpp
+//               Topic   : [Array]
+//               Problem :👉👉 [4]
+//               Want    :ChangeInputArr*2.cpp               
+// ================================================
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<< "Enter number " ;
+    cin>> n;
+    return 0 ;
+}
+
