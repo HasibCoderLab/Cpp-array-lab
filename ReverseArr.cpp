bdb839e3e568dd27f371e0acb897804d06250103
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-08 00:04:08 +0600

    create a file for practice Array

--- ReverseArr.cpp
+++ ReverseArr.cpp
@@ -2,18 +2,19 @@
 //               Language: C++ / Cpp
 //               Topic   : [Array]
 //               Problem :👉👉 [6]
-//               Want    : ReverseArr                  
+//               Want    : ReverseArr
 // ================================================
 #include <iostream>
 using namespace std;
-int main(){
-    int arr[5] = { 2, 4, 3, 6,1};
+int main()
+{
+    int arr[5] = {2, 4, 3, 6, 1};
     cout << "Array in reverse: ";
-   for (int i = 4; i >= 0; i--){
-    cout<<arr[i] << " ";
-   }cout<<endl;
-  
-   
-   
-    return 0 ;
+    for (int i = 4; i >= 0; i--)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
 }
