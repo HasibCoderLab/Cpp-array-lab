24fcfef58b8f40dbc65ab80bd76dccab3578931a
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-07 23:42:30 +0600

    upload

--- InputArr*2.cpp
+++ InputArr*2.cpp
@@ -2,14 +2,23 @@
 //               Language: C++ / Cpp
 //               Topic   : [Array]
 //               Problem :👉👉 [4]
-//               Want    : InputArr*2.cpp                    
+//               Want    : InputArr*2.cpp
 // ================================================
 #include <iostream>
 using namespace std;
-int main(){
-    int n;
-    cout<< "Enter number " ;
-    cin>> n;
-    return 0 ;
-}
+int main()
+{
+    int arr[5];
+    cout << "Enter 5  numbers ";
+    for (int i = 0; i < 5; i++)
+    {
+        cin >> arr[i];
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        cout << arr[i] * 2 << " ";
+    }
+    cout << endl;
 
+    return 0;
+}
