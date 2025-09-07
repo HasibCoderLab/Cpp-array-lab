062662e79a193ebf899a19e3ee4c916d0994cafc
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-07 23:29:00 +0600

    upload

--- InputArr.cpp
+++ InputArr.cpp
@@ -7,8 +7,17 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int n;
-    cout<< "Enter number " ;
-    cin>> n;
+    int arr[5];
+     cout<< "Enter  5 numbers :  " <<endl ;
+   for (int i = 0; i < 5; i++)
+   {
+    cin>> arr[i];
+   }
+   for (int i = 0; i < 5; i++)
+   {
+   cout<<arr[i] <<" " <<endl;
+   }
+   
+   
     return 0 ;
 }
