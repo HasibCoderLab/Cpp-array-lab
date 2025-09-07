ed923844dda0c9b242ef3fdd3c54cd10640ec9c6
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-07 23:53:46 +0600

    upload

--- ChangeInputArr*2.cpp
+++ ChangeInputArr*2.cpp
@@ -7,9 +7,22 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int n;
-    cout<< "Enter number " ;
-    cin>> n;
+    int arr[5];
+    cout<< "Enter 5 numbers " ;
+    for(int i = 0 ;i<5 ; i++){
+        cin>>arr[i];
+    }
+    for (int i = 0; i <5; i++)
+    {
+       cout<<arr[i] <<" " ;
+    }cout<<endl;
+         arr[0] = 100;
+    for (int i = 0; i <5; i++)
+    {
+       cout<<arr[i] <<" " ;
+    }
+    
+    
     return 0 ;
 }
 
