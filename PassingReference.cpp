fecbe7a6981125c7294acb0ae7e4d382e6b7963f
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-08 05:52:22 +0600

    upload

--- PassingReference.cpp
+++ PassingReference.cpp
@@ -1,6 +1,27 @@
 // ================================================
 //               Language: C++ / Cpp
 //               Topic   : [Array]
-//               Problem :👉👉 [6]
+//               Problem :👉👉 [7]
 //               Want    : PassingReference
 // ================================================
+#include <iostream>
+using namespace std;
+void change(int x[]){
+    x[0] = 10;
+
+}
+int main(){
+   int arr[4] = {1,2,3,4};
+   for (int i = 0; i <=4; i++)
+   {
+   cout<<arr[i] <<" ";
+   }cout<< endl;
+   change(arr) ;
+   for (int i = 0; i <=4; i++)
+   {
+   cout<<arr[i] << " " ;
+   }cout<<endl;
+   
+   
+    return 0;
+}
