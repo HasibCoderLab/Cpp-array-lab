2517d57f06e96c716a109970cccfc90ddef5090e
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-07 22:25:10 +0600

    upload

---
+++ Syntax.cpp
@@ -0,0 +1,15 @@
+// ================================================
+//               Language: C++ / Cpp
+//               Topic   : [Array]
+//               Problem : 👉👉 [1] 
+//               Want    : Syntax                     
+// ================================================
+#include <iostream>
+using namespace std;
+int main(){
+    int arr[5]; // Declaration
+    arr[0] = 3; //Set
+    arr[2] = 4;
+    cout<< arr[2];
+    return 0 ;
+}
