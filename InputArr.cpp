4fc4a0c6cd5b3b6dd835cc720aa69f723d059f1c
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-07 23:19:00 +0600

    create a file for practice Array

---
+++ InputArr.cpp
@@ -0,0 +1,14 @@
+// ================================================
+//               Language: C++ / Cpp
+//               Topic   : [Array]
+//               Problem :👉👉 [3]
+//               Want    : inputArr                  
+// ================================================
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<< "Enter number " ;
+    cin>> n;
+    return 0 ;
+}
