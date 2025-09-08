0aa23fda719708da8077171d557dd257dc593edb
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-08 06:00:21 +0600

    upload

--- CakculateSumOfArr.cpp
+++ CakculateSumOfArr.cpp
@@ -3,5 +3,16 @@
 //               Topic   : [Array]
 //               Problem :👉👉 [8]
 //               Want    : CakculateSumOfArr.cpp
-
 // ================================================
+#include <iostream>
+using namespace std;
+int main(){
+int arr[] = {1,5,4,5,6,7,10};
+int sum = 0;
+for (int i = 0; i <=arr[i]; i++)
+{
+   sum +=arr[i];
+//    cout<<arr[i]<<" ";
+}cout<<sum<<" "<<endl;;
+
+}
