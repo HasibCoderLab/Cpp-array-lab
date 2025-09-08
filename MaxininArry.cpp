e77fd253b844aa85b3b48ce680fc516345cfd1c4
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-09-08 14:22:02 +0600

    update

--- MaxininArry.cpp
+++ MaxininArry.cpp
@@ -9,7 +9,7 @@ using namespace std;
 int main(){
 int arr[] = {2,4,3,4,-1,5,6,3,};
      int n = sizeof(arr) / 4 ;
-     int mx = arr[0];
+     int mx = arr[0];/// INT_MAx
      for (int i = 1; i < n; i++)
      {
        if (arr[i]>mx)
